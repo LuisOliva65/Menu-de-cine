96021b93e2af8ee5dbcf5197889c59ebcf5198a2
LuisOliva65 <65673151+LuisOliva65@users.noreply.github.com>
2020-05-28 14:49:50 -0500

    Add files via upload

---
+++ menucine.cpp
@@ -0,0 +1,65 @@
+#include <stdio.h>
+
+int main()
+{
+    int opcion;
+    
+    while(opcion!=10)
+    {
+        printf("===Bienvenido a CineWich===");
+        printf("\n Elija una opcion");
+        printf("\n 1 Catelera");
+        printf("\n 2 Funciones");
+        printf("\n 3 Salas");
+        printf("\n 4 Dulcería");
+        printf("\n 5 Membresía");
+        printf("\n 6 Código de descuento");
+        printf("\n 7 Pago");
+        printf("\n 8 Sumar puntos a la  membresía");
+        printf("\n 9 Imprimir boleto");
+        printf("\n 10 Salir\n");
+        printf("Su opcion: ");
+        scanf("%d", &opcion);
+        
+        if(opcion==1)
+        {
+        
+        }
+        if (opcion==2)
+        {
+        
+        }
+        if (opcion==3)
+        {
+            
+        }
+        if (opcion==4)
+        {
+        
+        }
+        if (opcion==5)
+        {
+        
+        }
+        if(opcion==6)
+        {
+        
+        }
+        if (opcion==7)
+        {
+        
+        }
+        if (opcion==8)
+        {
+            
+        }
+        if (opcion==9)
+        {
+        
+        }
+        if (opcion==10)
+        {
+        
+        }
+    }
+}
